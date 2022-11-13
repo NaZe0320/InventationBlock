d6d9a457d72e0e752d404c6906fcf6cf81497f4f
NaZe0320 <jisong1226@gmail.com>
2022-11-14 01:09:38 +0900

    Register 수정 Login 오류 수정

--- app/src/main/java/com/oneandonly/inventationblock/ui/activity/RegisterActivity.kt
+++ app/src/main/java/com/oneandonly/inventationblock/ui/activity/RegisterActivity.kt
@@ -26,7 +26,12 @@ class RegisterActivity : AppCompatActivity() {
                     when(fragment.tag) {
                         "Register" -> {
                             Log.d("Register_Activity","${supportFragmentManager.fragments}")
-                            changeFragment(RegisterCompleteFragment(),"RegisterComplete")
+
+                            if (true) { //TODO(성공했을 때)
+                                changeFragment(RegisterCompleteFragment(),"RegisterComplete")
+                            } else {
+                            }
+
                         }
                         "RegisterComplete" -> {
                             finish()
@@ -34,6 +39,10 @@ class RegisterActivity : AppCompatActivity() {
                     }
                 }
             }
+            /*TODO(버튼 클릭 시 회원가입 정보를 Fragment로부터 받아와서 (ViewModel에서 처리하면 될 듯하다.
+               회원가입 성공일 때 State를 Success로 변경하여 다음 화면으로 넘어가기
+               ViewModel에서 Model과 통신하기)
+            */
 
         }
 
