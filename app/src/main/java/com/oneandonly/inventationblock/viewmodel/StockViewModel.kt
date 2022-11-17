c5185e1fe1604f4ab45c540f0f68de673b288348
NaZe0320 <jisong1226@gmail.com>
2022-11-17 13:04:51 +0900

    item_stock 구현 (seekBar canvas 로 교체 예정)

--- app/src/main/java/com/oneandonly/inventationblock/viewmodel/StockViewModel.kt
+++ app/src/main/java/com/oneandonly/inventationblock/viewmodel/StockViewModel.kt
@@ -1,6 +1,13 @@
 package com.oneandonly.inventationblock.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class StockViewModel: ViewModel() {
+
+    var name: MutableLiveData<String> = MutableLiveData()
+    var stock: MutableLiveData<Int> = MutableLiveData()
+    var unit: MutableLiveData<String> = MutableLiveData()
+    var fixed: MutableLiveData<Boolean> = MutableLiveData()
+    var expiration: MutableLiveData<Int> = MutableLiveData()
 }
