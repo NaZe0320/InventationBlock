abdabf42b1ef9bfca5043f349eb704936eeb04b3
NaZe0320 <jisong1226@gmail.com>
2022-11-17 13:07:06 +0900

    Stock Model 생성

---
+++ app/src/main/java/com/oneandonly/inventationblock/datasource/model/data/Stock.kt
@@ -0,0 +1,8 @@
+package com.oneandonly.inventationblock.datasource.model.data
+
+data class Stock(
+    var name:String,
+    var stock: Int,
+    var fixed: Boolean,
+    var unit: String
+)
---
+++ app/src/main/java/com/oneandonly/inventationblock/ui/adapter/StockAdapter.kt
@@ -0,0 +1,4 @@
+package com.oneandonly.inventationblock.ui.adapter
+
+class StockAdapter {
+}
