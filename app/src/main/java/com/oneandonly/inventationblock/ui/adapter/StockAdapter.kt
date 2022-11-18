b665de061eebf3efddfe0cc936ab9d937a47ca9d
NaZe0320 <jisong1226@gmail.com>
2022-11-18 18:06:28 +0900

    Stock View Holder 생성

--- app/src/main/java/com/oneandonly/inventationblock/ui/adapter/StockAdapter.kt
+++ app/src/main/java/com/oneandonly/inventationblock/ui/adapter/StockAdapter.kt
@@ -1,4 +1,15 @@
 package com.oneandonly.inventationblock.ui.adapter
 
+import androidx.recyclerview.widget.RecyclerView
+import com.oneandonly.inventationblock.databinding.ItemStockBinding
+import com.oneandonly.inventationblock.datasource.model.data.Stock
+
 class StockAdapter {
+
+    inner class StockViewHolder(private val binding: ItemStockBinding):RecyclerView.ViewHolder(binding.root) {
+        fun bind(stock: Stock) {
+            binding.stock = stock
+        }
+    }
+    
 }
