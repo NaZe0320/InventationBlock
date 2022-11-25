166f54c5316e7d03ec65cfa00700e7e7728f7604
NaZe0320 <jisong1226@gmail.com>
2022-11-25 15:01:16 +0900

    개별 stock activity - menu adapter 생성

--- app/src/main/java/com/oneandonly/inventationblock/ui/adapter/MenuAdapter.kt
+++ app/src/main/java/com/oneandonly/inventationblock/ui/adapter/MenuAdapter.kt
@@ -1,4 +1,33 @@
 package com.oneandonly.inventationblock.ui.adapter
 
-class MenuAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.oneandonly.inventationblock.databinding.ItemHistoryBinding
+import com.oneandonly.inventationblock.databinding.ItemMenuBinding
+import com.oneandonly.inventationblock.datasource.model.data.History
+
+class MenuAdapter(private val items: LiveData<ArrayList<String>>):RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
+
+    inner class MenuViewHolder(private val binding: ItemMenuBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(menu: String) {
+            binding.menu = menu
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
+        return MenuViewHolder((ItemMenuBinding.inflate(LayoutInflater.from(parent.context),parent,false)))
+    }
+
+    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
+        items.value?.get(position)?.let {
+            holder.bind(it)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.value?.size?:0
+    }
 }
