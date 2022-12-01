258b254f03030405239369b235ed242cc424d0f1
NaZe0320 <jisong1226@gmail.com>
2022-12-01 18:19:32 +0900

    datePicker Alert 완성

--- app/src/main/java/com/oneandonly/inventationblock/ui/activity/MenuAddActivity.kt
+++ app/src/main/java/com/oneandonly/inventationblock/ui/activity/MenuAddActivity.kt
@@ -1,9 +1,14 @@
 package com.oneandonly.inventationblock.ui.activity
 
+import android.R.style.*
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
+import android.widget.DatePicker
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.oneandonly.inventationblock.R
@@ -15,9 +20,8 @@ import com.oneandonly.inventationblock.ui.activity.MainActivity.Companion.search
 import com.oneandonly.inventationblock.ui.adapter.SearchDropDownAdapter
 import com.oneandonly.inventationblock.viewmodel.StockViewModel
 import com.oneandonly.inventationblock.viewmodel.factory.StockFactory
-import java.text.DateFormat
 import java.text.SimpleDateFormat
-import java.util.Calendar
+import java.util.*
 
 class MenuAddActivity : AppCompatActivity() {
 
@@ -37,7 +41,6 @@ class MenuAddActivity : AppCompatActivity() {
         val date = dateToString(mCurrentTime.time)
 
         setViewModel()
-
         uiSetting(date)
     }
 
@@ -69,8 +72,32 @@ class MenuAddActivity : AppCompatActivity() {
                 buyDate = format.format(mCurrentTime.time),
                 reason = "직접 등록"
             )
-        } //TODO(날짜 기능 만들기)
+            showDialog()
+        }
 
+        setSearchEdit()
+        setDatePicker()
+    }
+
+    private fun showDialog() {
+        AlertDialog.Builder(this, Theme_DeviceDefault_Light_Dialog_Alert)
+            .setTitle("등록 완료")
+            .setMessage("등록이 완료되었습니다")
+            .setPositiveButton("홈으로 이동하기") { p0, p1 ->
+                Log.d("dialog", "positive $p0 $p1")
+                val intent = Intent(this@MenuAddActivity, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
+            }
+            .setNeutralButton("이어서 등록하기") { p0, p1 ->
+                Log.d("dialog", "neutral $p0 $p1")
+                clear()
+            }
+            .create()
+            .show()
+    }
+
+    private fun setSearchEdit() {
         val adapter = SearchDropDownAdapter(this, R.layout.item_dropdown2, searchList)
         binding.stockSearchEdit.setAdapter(adapter)
         binding.stockSearchEdit.threshold = 1
@@ -90,6 +117,41 @@ class MenuAddActivity : AppCompatActivity() {
                 stockSelectedChange()
             }
         }
+    }
+
+    private fun setDatePicker() {
+
+        val mcurrentTime = Calendar.getInstance()
+        val year = mcurrentTime.get(Calendar.YEAR)
+        val month = mcurrentTime.get(Calendar.MONTH)
+        val day = mcurrentTime.get(Calendar.DAY_OF_MONTH)
+
+        val datePicker = DatePickerDialog(this, R.style.DatePickerStyle, object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+                val calendar = Calendar.getInstance()
+
+                calendar.set(year,month,dayOfMonth)
+
+                val date = calendar.time
+                val dateFormat = SimpleDateFormat("EE", Locale.getDefault())
+                val dayName: String = dateFormat.format(date)
+
+                binding.dateEdit.text = "$year.${month+1}.$dayOfMonth.$dayName"
+            }
+        }, year, month, day)
+        binding.dateEdit.setOnClickListener {
+            datePicker.show()
+        }
+
+    }//TODO(날짜 기능 만들기)
+
+    private fun clear() {
+        binding.amountEdit.text.clear()
+        binding.stockSearchEdit.text.clear()
+        stockSelectedChange()
+        binding.amountEdit.clearFocus()
+        sid = 0
     }
 
     private fun stockSelectedChange() {
